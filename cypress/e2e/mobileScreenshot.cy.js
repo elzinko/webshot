69cae772faf5f13f55015ba1559134b7191d4c72
Thomas Couderc <thomas.couderc@gmail.com>
2023-09-16 02:30:55 +0200

    e2e: can execute eée test against different hosts

--- cypress/e2e/mobileScreenshot.cy.js
+++ cypress/e2e/mobileScreenshot.cy.js
@@ -1,6 +1,6 @@
 describe('The API', () => {
   it('returns mobile screenshot', () => {
-    cy.request('http://localhost:3000/api/github.fr?device=mobile').should((response) => {
+    cy.request(`${Cypress.env('host')}/api/github.fr?device=mobile`).should((response) => {
       expect(response.status).to.eq(200)
       expect(response.headers['content-type']).to.eq('image/png')
     })
