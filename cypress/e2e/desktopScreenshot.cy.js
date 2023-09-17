c02fd21495d0b9bfadd54badb18765f74e0f1a8f
Thomas Couderc <thomas.couderc@gmail.com>
2023-09-18 01:07:55 +0200

    fix: limit destop e2e tests due to some hazardous 504 responses

--- cypress/e2e/desktopScreenshot.cy.js
+++ cypress/e2e/desktopScreenshot.cy.js
@@ -1,11 +1,5 @@
 describe('The API', () => {
-  it('returns desktop screenshot', () => {
-    cy.request(`${Cypress.env('host')}/api/github.fr`).should((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.headers['content-type']).to.eq('image/png')
-    })
-  })
-  it('returns desktop screenshot', () => {
+  it('returns desktop screenshot with device parameter', () => {
     cy.request(`${Cypress.env('host')}/api/github.fr?device=desktop`).should((response) => {
       expect(response.status).to.eq(200)
       expect(response.headers['content-type']).to.eq('image/png')
